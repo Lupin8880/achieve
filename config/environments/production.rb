a5ebd8d25e58d688cc24b6390b64a5a0d7759e0a
Daisuke TABUCHI <t-da@suou.waseda.jp>
2017-02-25 12:47:34 +0000

    環境変数変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,8 @@ config.action_mailer.default_url_options = { host: 'https://damp-basin-41335.her
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings =
 {
- user_name: ENV['app59467657@heroku.com'],
- password: ENV['rq7ylwed3705'],
+ user_name: ENV['SENDGRID_PASSWORD'],
+ password: ENV['SENDGRID_USERNAME'],
  domain: "heroku.com",
  address: "smtp.sendgrid.net",
  port: 587,
