a8cfa70c57edcb2e85644fadc864536bf697361f
Daisuke Tabuchi <t-da@suou.waseda.jp>
2017-03-25 12:53:35 +0000

    201703252153

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -20,7 +20,6 @@ class MessagesController < ApplicationController
            @messages.last.read = true
         end
       end
-            @message = @conversation.messages.build
   end
 
 def create
